7198c98f4a3bd73046b4f0889ee971e2b8a98ee2
githubhyatri <126131098+githubhyatri@users.noreply.github.com>
2023-05-24 23:14:03 -0700

    Added venmo
    Added Venmo

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def create_checkout():
         return redirect(url_for('new_checkout'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(host='127.0.0.1', port=PORT, debug=True)
